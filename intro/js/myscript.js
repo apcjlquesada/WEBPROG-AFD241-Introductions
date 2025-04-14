5618c8d7e603ac87b062ad40b50332cdf7769847
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-04-14 10:47:33 +0800

    updated js function

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,8 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
+    
+    document.getElementById("p2").style.color = "blue";
+    document.getElementById("p2").style.fontFamily = "Arial";
+    document.getElementById("p2").style.fontSize = "larger";
 }
 
 
-document.getElementById("p2").style.color = "blue";
-document.getElementById("p2").style.fontFamily = "Arial";
-document.getElementById("p2").style.fontSize = "larger";
